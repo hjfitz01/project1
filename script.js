63b19dcc6128da8f44c953cf3c5da39cf831662c
hjfitz01 <hannahfitzgerald@icloud.com>
2016-03-06 15:42:04 -0500

    subtopics open up individually

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-
-
-
 $(document).ready(function(){
     $(".topic-button").click(function (){
-        $(".subtopics").toggleClass("closed");
+        $(this).parent().find('.subtopics').toggleClass("closed");
     });
 });
-
-
